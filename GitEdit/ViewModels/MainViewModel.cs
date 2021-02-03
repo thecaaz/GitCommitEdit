cceffe10b897ee4693da859d1c206a93887d6494
thecaaz <o.arth@live.de>
2021-02-03 22:48:10 +0100

    Simple MVVM

---
+++ GitEdit/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitEdit.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string output;
+        public string Output
+        {
+            get { return output; }
+            set
+            {
+                output = value;
+                RaisePropertyChangedEvent(nameof(Output));
+            }
+        }
+
+        public MainViewModel()
+        {
+            var repoPath = @"D:\Users\oarth\Documents\GitHub\GitEdit";
+
+            using (var repo = new Repository(repoPath))
+            {
+                AppendLine("Branches");
+                AppendLine("-----------------");
+                var branches = repo.Branches;
+                foreach (var b in branches)
+                {
+                    AppendLine(b.FriendlyName);
+
+                    foreach (var commit in b.Commits)
+                    {
+                        AppendLine(commit.ToString());
+                    }
+                }
+                AppendLine("-----------------");
+            }
+        }
+
+        private void AppendLine(string content)
+        {
+            Output += content + Environment.NewLine;
+        }
+    }
+}
---
+++ GitEdit/ViewModels/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GitEdit.ViewModels
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
