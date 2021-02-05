56f6d7609f07b1d7fbe0c4bb402c25bda97c3e8a
thecaaz <o.arth@live.de>
2021-02-05 15:00:29 +0100

    Add some directory select logic / validation Add material design

--- GitEdit/ViewModels/MainViewModel.cs
+++ GitEdit/ViewModels/MainViewModel.cs
@@ -1,13 +1,35 @@
 ﻿using LibGit2Sharp;
+using Ookii.Dialogs.Wpf;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Windows.Input;
 
 namespace GitEdit.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
-        private string output;
+        private string gitPath = "";
+        public string GitPath
+        {
+            get { return gitPath; }
+            set
+            {
+                gitPath = value;
+                RaisePropertyChangedEvent(nameof(GitPath));
+                if (GitPathIsValid())
+                {
+                    ChangeGitDirectory();
+                }
+                else
+                {
+                    Output = "Directory Path is not valid a valid git repository";
+                }
+            }
+        }
+
+        private string output = "";
         public string Output
         {
             get { return output; }
@@ -18,8 +40,8 @@ namespace GitEdit.ViewModels
             }
         }
 
-        private Branch activeBranch;
-        public Branch ActiveBranch
+        private Branch? activeBranch;
+        public Branch? ActiveBranch
         {
             get { return activeBranch; }
             set
@@ -31,9 +53,50 @@ namespace GitEdit.ViewModels
 
         public MainViewModel()
         {
-            var repoPath = @"D:\Users\oarth\Documents\GitHub\GitEdit";
+            GitPath = @"D:\Users\oarth\Documents\GitHub\GitEdit";
+        }
 
-            using (var repo = new Repository(repoPath))
+        public ICommand SelectDirectoryCommand
+        {
+            get { return new DelegateCommand(SelectDirectory); }
+        }
+
+        private void SelectDirectory()
+        {
+            var a = new VistaFolderBrowserDialog();
+
+            if (DirectoryPathIsValid())
+            {
+                a.SelectedPath = GitPath;
+            }
+
+            if (a.ShowDialog() == true)
+            {
+                GitPath = a.SelectedPath;
+            }
+        }
+
+        private bool GitPathIsValid()
+        {
+            if (DirectoryPathIsValid())
+            {
+                try
+                {
+                    using (_ = new Repository(GitPath)) { }
+                }
+                catch (RepositoryNotFoundException)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ChangeGitDirectory()
+        {
+            Output = "";
+
+            using (var repo = new Repository(GitPath))
             {
                 AppendLine("Branches");
                 AppendLine("-----------------");
@@ -42,14 +105,14 @@ namespace GitEdit.ViewModels
                 {
                     ActiveBranch = b;
                     AppendLine(b.FriendlyName);
-
-                    foreach (var commit in b.Commits)
-                    {
-                        AppendLine(commit.ToString());
-                    }
                 }
                 AppendLine("-----------------");
             }
+        }
+
+        private bool DirectoryPathIsValid()
+        {
+            return Directory.Exists(gitPath);
         }
 
         private void AppendLine(string content)
